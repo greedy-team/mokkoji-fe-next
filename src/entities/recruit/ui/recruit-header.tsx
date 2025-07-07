8ba243cbe1678a410165b66cfa6423c3eb4a72ec
신혁수 <122273080+sins051301@users.noreply.github.com>
2025-07-07 16:00:58 +0900

    [#7] feat: affliation별 api 연동 완료

--- src/entities/recruit/ui/recruit-header.tsx
+++ src/entities/recruit/ui/recruit-header.tsx
@@ -1,21 +1,11 @@
-import NavButton from '@/shared/ui/nav-button';
+import NavSection from './nav-section';
 
 function RecruitHeader() {
   return (
     <header className="mb-15">
       <nav className="mb-5 flex flex-row justify-between">
         <h1 className="text-xl font-bold text-[#00E457]">모집 공고</h1>
-        <div className="mr-12 flex flex-row gap-4">
-          <NavButton
-            label="중앙동아리"
-            href="/recruit?affiliation=CENTRAL_CLUB"
-          />
-          <NavButton
-            label="기타동아리"
-            href="/recruit?affiliation=DEPARTMENT_CLUB"
-          />
-          <NavButton label="소모임" href="/recruit" />
-        </div>
+        <NavSection />
       </nav>
       <h2 className="mt-2 mb-3 text-3xl font-bold text-[474747]">
         관심 있는 동아리의 최신공고를
--- src/views/recruit/recruit-page.tsx
+++ src/views/recruit/recruit-page.tsx
@@ -1,11 +1,12 @@
 import RecruitHeader from '@/entities/recruit/ui/recruit-header';
 import RecruitItemList from '@/widgets/recruit/ui/recruit-item-list';
+import { RecruitItemListProps } from '@/widgets/recruit/model/type';
 
-function RecruitPage() {
+function RecruitPage({ searchParams }: RecruitItemListProps) {
   return (
     <div>
       <RecruitHeader />
-      <RecruitItemList />
+      <RecruitItemList searchParams={searchParams} />
     </div>
   );
 }

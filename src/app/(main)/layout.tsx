ca6928e69d90b17a265c4c0217425297384bb3a5
ChangwooJ <wjdckddn99@naver.com>
2025-07-28 22:02:32 +0900

    [no-issue] refactor: 메인 레이아웃 client-provider로 분리 및 server components로 변경

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,9 +1,4 @@
-'use client';
-
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Header from '@/shared/ui/Header';
-import { SessionProvider } from 'next-auth/react';
+import ClientProviders from '@/_providers/client-porvider';
 
 export default function MainLayout({
   children,
@@ -11,21 +6,10 @@ export default function MainLayout({
   children: React.ReactNode;
 }) {
   return (
-    <SessionProvider>
-      <Header />
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
-      {children}
-    </SessionProvider>
+    <html lang="ko">
+      <body>
+        <ClientProviders>{children}</ClientProviders>
+      </body>
+    </html>
   );
 }

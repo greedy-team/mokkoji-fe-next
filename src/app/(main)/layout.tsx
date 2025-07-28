beb1cd52652b293bf53dfcb4a996745bab245344
ChangwooJ <wjdckddn99@naver.com>
2025-07-28 22:36:20 +0900

    [#35] feat: footer 추가

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,6 +4,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Header from '@/shared/ui/Header';
 import { SessionProvider } from 'next-auth/react';
+import Footer from '@/shared/ui/Footer';
 
 export default function MainLayout({
   children,
@@ -12,20 +13,23 @@ export default function MainLayout({
 }) {
   return (
     <SessionProvider>
-      <Header />
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
-      {children}
+      <div className="flex min-h-screen flex-col">
+        <Header />
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+        <main className="flex-grow">{children}</main>
+        <Footer />
+      </div>
     </SessionProvider>
   );
 }

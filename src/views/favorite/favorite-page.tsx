c2e4c648d37162673f53b80fb1effe6ae0aeacc3
신혁수 <122273080+sins051301@users.noreply.github.com>
2025-07-28 16:49:45 +0900

    [#32] feat: 로그인 기능 구현

---
+++ src/views/favorite/favorite-page.tsx
@@ -0,0 +1,13 @@
+import FavoriteDateSection from '@/widgets/favorite/ui/favorite-date-section';
+import FavoriteItemSection from '@/widgets/favorite/ui/favorite-item-section';
+
+function FavoritePage() {
+  return (
+    <main>
+      <FavoriteItemSection />
+      <FavoriteDateSection />
+    </main>
+  );
+}
+
+export default FavoritePage;

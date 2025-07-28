a96836ce9ad19f2c1543b9ea733519b7697fb32c
ChangwooJ <wjdckddn99@naver.com>
2025-07-28 18:34:47 +0900

    [#28] refactor: api 요청 header 제거

--- src/features/recruit-detail/api/postFavoriteClub.ts
+++ src/features/recruit-detail/api/postFavoriteClub.ts
@@ -1,13 +1,7 @@
 import serverApi from '@/shared/api/server-api';
 
 export default async function postFavoriteClub(clubId: number) {
-  const response = await serverApi
-    .post(`favorites/${clubId}`, {
-      headers: {
-        Authorization: 'Bearer ', // 나중에 여기에 로그인 토큰 추가
-      },
-    })
-    .json();
+  const response = await serverApi.post(`favorites/${clubId}`).json();
 
   return response;
 }

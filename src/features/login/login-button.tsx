77bca04c3908b81c47a0e02e2ad51f30669b6672
신혁수 <122273080+sins051301@users.noreply.github.com>
2025-07-29 21:57:14 +0900

    [#42] feat: 전체 동아리 페이지 구현

--- src/features/login/login-button.tsx
+++ src/features/login/login-button.tsx
@@ -5,7 +5,7 @@ import KakaoIcon from '@/shared/ui/kakao-icon';
 
 function LoginButton() {
   const handleKakaoLogin = () => {
-    const kakaoAuthURL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}`;
+    const kakaoAuthURL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URL}`;
     window.location.href = kakaoAuthURL;
   };
   return (

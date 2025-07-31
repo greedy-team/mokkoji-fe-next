666477da1ddc47534eb214cb5c1163eb165f2dfa
ChangwooJ <wjdckddn99@naver.com>
2025-07-31 23:11:54 +0900

    [#33] feat: 카테고리 소개 텍스트&배경 배너 추가

--- src/widgets/home/ui/feature-intoduce-widget.tsx
+++ src/widgets/home/ui/feature-intoduce-widget.tsx
@@ -1,12 +1,18 @@
+import CategoryCard from '@/entities/home/ui/category-card';
+import CategoryTextCard from '@/entities/home/ui/category-text-card';
 import CommentCard from '@/entities/home/ui/comment-card';
 import CommentTextCard from '@/entities/home/ui/comment-text-card';
 
 function FeatureIntroduceWidget() {
   return (
-    <div className="flex flex-col gap-40">
-      <div className="flex h-[550px] flex-col items-center pt-20">
+    <div className="flex flex-col gap-20">
+      <div className="flex h-[550px] flex-col items-center pt-40">
         <CommentTextCard />
         <CommentCard />
+      </div>
+      <div className="relative flex h-[550px] w-full items-center pt-20">
+        <CategoryTextCard />
+        <CategoryCard />
       </div>
     </div>
   );

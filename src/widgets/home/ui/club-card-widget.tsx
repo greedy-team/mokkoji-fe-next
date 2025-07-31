ab0e77de05b09781bc7e5709342279a26295293f
ChangwooJ <wjdckddn99@naver.com>
2025-07-31 17:54:59 +0900

    [#33] feat: 모집 공고 소개 카드 텍스트 추가

--- src/widgets/home/ui/club-card-widget.tsx
+++ src/widgets/home/ui/club-card-widget.tsx
@@ -1,6 +1,7 @@
 import CardSilder from '@/features/home/ui/club-card';
 import ClubTextCard from '@/entities/home/ui/club-text-card';
 import getRecruitList from '@/widgets/recruit/api/getRecruitList';
+import RecruitTextCard from '@/entities/home/ui/recruit-text-card';
 
 async function ClubCardWidget() {
   const data = await getRecruitList({
@@ -13,9 +14,14 @@ async function ClubCardWidget() {
   });
 
   return (
-    <div id="scroll-target" className="flex h-[300px] items-center pt-20">
-      <ClubTextCard />
-      <CardSilder data={data} />
+    <div className="flex flex-col gap-40">
+      <div id="scroll-target" className="flex h-[300px] items-center pt-20">
+        <ClubTextCard />
+        <CardSilder data={data} />
+      </div>
+      <div className="flex h-[300px] w-full items-center pt-20">
+        <RecruitTextCard />
+      </div>
     </div>
   );
 }

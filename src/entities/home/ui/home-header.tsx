159302ae5e962e2097c83f4c18947042150661b5
ChangwooJ <wjdckddn99@naver.com>
2025-08-01 13:17:06 +0900

    [#33] refactor: 메인 페이지 일부 ui 개선

--- src/entities/home/ui/home-header.tsx
+++ src/entities/home/ui/home-header.tsx
@@ -5,12 +5,18 @@ function HomeHeader() {
         Club, set, go!
       </h1>
       <p className="text-3xl font-bold">
-        세종대의 다양한 <span className="text-[#00E804]">동아리</span>를
-        한곳에서 만나보세요.
+        세종대의 다양한{' '}
+        <span className="bg-[linear-gradient(to_right,_#00E804_0%,_#3AE2EB_73%,_#3AA1EB_100%)] bg-clip-text text-transparent">
+          동아리
+        </span>
+        를 한곳에서 만나보세요.
       </p>
       <p className="text-3xl font-bold">
         관심 있는 동아리를 찾고,{' '}
-        <span className="text-[#00E804]">새로운 사람들</span>과 함께하세요!
+        <span className="bg-[linear-gradient(to_right,_#00E804_0%,_#3AE2EB_73%,_#3AA1EB_100%)] bg-clip-text text-transparent">
+          새로운 사람들
+        </span>
+        과 함께하세요!
       </p>
     </header>
   );

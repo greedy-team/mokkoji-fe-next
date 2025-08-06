770d8556f7c9dceca50241fa2f7f7048b27daf63
신혁수 <122273080+sins051301@users.noreply.github.com>
2025-08-06 19:01:59 +0900

    [#40] fix: suspense 추가

--- src/views/recruit/ui/recruit-page.tsx
+++ src/views/recruit/ui/recruit-page.tsx
@@ -2,12 +2,16 @@ import RecruitHeader from '@/entities/recruit/ui/recruit-header';
 import RecruitItemList from '@/widgets/recruit/ui/recruit-item-list';
 import { RecruitItemListProps } from '@/widgets/recruit/model/type';
 import ScrollTopButton from '@/features/recruit/ui/scroll-top-button';
+import { Suspense } from 'react';
+import SharedLoading from '@/shared/ui/loading';
 
 function RecruitPage({ searchParams }: RecruitItemListProps) {
   return (
     <>
       <RecruitHeader />
-      <RecruitItemList searchParams={searchParams} />
+      <Suspense fallback={<SharedLoading />}>
+        <RecruitItemList searchParams={searchParams} />
+      </Suspense>
       <ScrollTopButton />
     </>
   );

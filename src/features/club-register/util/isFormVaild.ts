4bb3eb2e3b7041d2eab98475206591a755932dce
ChangwooJ <wjdckddn99@naver.com>
2025-08-07 01:48:39 +0900

    [#62] refactor: afiliation enum 변경&인스타그램 null 허용

--- src/features/club-register/util/isFormVaild.ts
+++ src/features/club-register/util/isFormVaild.ts
@@ -4,10 +4,12 @@ export default function isFormValid(
   { formData, errors }: StateProp,
   fields: FormField[],
 ) {
-  const allFilled = fields.every((field) => {
-    const val = formData[field.name as keyof typeof formData];
-    return typeof val === 'string' && val.trim() !== '';
-  });
+  const allFilled = fields
+    .filter((field) => field.name !== 'instagram')
+    .every((field) => {
+      const val = formData[field.name as keyof typeof formData];
+      return typeof val === 'string' && val.trim() !== '';
+    });
 
   const noErrors = Object.values(errors).every((msg) => !msg);
   return allFilled && noErrors;

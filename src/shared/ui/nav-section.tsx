cff2e0b74a4ec8ee57dc3ba39c132574d37459e4
ChangwooJ <wjdckddn99@naver.com>
2025-08-08 22:41:39 +0900

    [#85] refactor: 모집 공고 페이지 반응형&이름 데이터 수정

--- src/shared/ui/nav-section.tsx
+++ src/shared/ui/nav-section.tsx
@@ -14,13 +14,13 @@ interface NavSectionProps {
 
 export default function NavSection({ href }: NavSectionProps) {
   return (
-    <div className="mr-12 flex flex-row gap-4">
+    <div className="flex flex-row lg:mr-12 lg:gap-4">
       {navItems.map(({ label, value }) => (
         <AffiliationNavButton
           key={value}
           label={label}
           href={`${href}?affiliation=${value}`}
-          navProps="pb-2 text-xs"
+          navProps="lg:pb-2 text-[10px] lg:text-xs"
           value={value}
         />
       ))}

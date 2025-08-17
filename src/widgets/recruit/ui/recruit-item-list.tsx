0e03d6ca6114cde3b0f09028d7e597d06f5019f3
신혁수 <122273080+sins051301@users.noreply.github.com>
2025-08-18 01:29:40 +0900

    [#98] fix: try catch 로직 다시 수정

--- src/widgets/recruit/ui/recruit-item-list.tsx
+++ src/widgets/recruit/ui/recruit-item-list.tsx
@@ -6,29 +6,27 @@ import { RecruitItemListProps } from '../model/type';
 import getClubRecruitList from '../api/getClubRecruitList';
 
 async function RecruitItemList({ searchParams }: RecruitItemListProps) {
-  let data;
-  try {
-    data = await getClubRecruitList({
-      page: Number((await searchParams).page || 1),
-      size: Number((await searchParams).size || 100),
-      affiliation: (
-        await searchParams
-      ).affiliation?.toUpperCase() as ClubAffiliation,
-    });
-    if (data.recruitments.length === 0) {
-      return (
-        <p className="mt-30 w-full text-center text-sm font-bold text-[#00E457]">
-          해당 동아리의 모집 공고가 없습니다.
-        </p>
-      );
-    }
-  } catch (error) {
+  const res = await getClubRecruitList({
+    page: Number((await searchParams).page || 1),
+    size: Number((await searchParams).size || 100),
+    affiliation: (
+      await searchParams
+    ).affiliation?.toUpperCase() as ClubAffiliation,
+  });
+  if (!res.ok) {
     return <ErrorBoundaryUi />;
+  }
+  if (res.data?.recruitments.length === 0) {
+    return (
+      <p className="mt-30 w-full text-center text-sm font-bold text-[#00E457]">
+        해당 동아리의 모집 공고가 없습니다.
+      </p>
+    );
   }
 
   return (
     <ul className="grid w-auto grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-      {data?.recruitments?.map((item) => (
+      {res.data?.recruitments?.map((item) => (
         <li key={item.id}>
           <Link href={`/recruit/${item.id}`}>
             <RecruitItem

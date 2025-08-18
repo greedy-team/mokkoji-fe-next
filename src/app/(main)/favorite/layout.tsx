954e8cee6bb196fd16e0996a22cef1bd21e290e6
신혁수 <122273080+sins051301@users.noreply.github.com>
2025-08-18 23:04:34 +0900

    [no-issue] fix: todo제거

--- src/app/(main)/favorite/layout.tsx
+++ src/app/(main)/favorite/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-import DevTodo from '@/shared/ui/dev-to-do';
 
 export const metadata: Metadata = {
   title: '모꼬지 | 즐겨찾기',
@@ -13,14 +12,6 @@ export default function Layout({
 }>) {
   return (
     <main className="flex flex-col items-center justify-center pt-10">
-      <DevTodo
-        id="favorite"
-        todos={['test3', 'test4']}
-        name="sins051301"
-        description="test"
-        x={688}
-        y={83}
-      />
       {children}
     </main>
   );

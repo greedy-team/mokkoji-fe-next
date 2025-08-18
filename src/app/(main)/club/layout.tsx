a3f880afa0b5451341d7753ed0b4f3623d81a5b5
신혁수 <122273080+sins051301@users.noreply.github.com>
2025-08-18 23:04:34 +0900

    [no-issue] fix: todo제거

--- src/app/(main)/club/layout.tsx
+++ src/app/(main)/club/layout.tsx
@@ -1,4 +1,3 @@
-import DevTodo from '@/shared/ui/dev-to-do';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -13,14 +12,6 @@ export default function Layout({
 }>) {
   return (
     <main className="flex flex-col items-center justify-center px-3 pt-2 sm:pt-2 lg:pt-10">
-      <DevTodo
-        id="club"
-        todos={['test1', 'test2']}
-        name="sins051301"
-        description="test"
-        x={688}
-        y={83}
-      />
       {children}
     </main>
   );

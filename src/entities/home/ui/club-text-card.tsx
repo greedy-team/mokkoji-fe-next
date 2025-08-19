0ca06a122b10b3c399528a06eb915c0998ac220a
ChangwooJ <wjdckddn99@naver.com>
2025-08-19 17:26:28 +0900

    [#103] refactor: 메인 페이지 반응형 리팩토링

--- src/entities/home/ui/club-text-card.tsx
+++ src/entities/home/ui/club-text-card.tsx
@@ -4,15 +4,17 @@ function ClubTextCard() {
   return (
     <div className="flex h-[50%] w-full flex-col justify-center lg:h-full lg:w-[50%]">
       <AnimateOnView animation="animate-fade-left">
-        <h4 className="text-2xl font-bold text-[#00E457]">전체 동아리</h4>
+        <h4 className="text-lg font-bold text-[#00E457] lg:text-2xl">
+          전체 동아리
+        </h4>
       </AnimateOnView>
       <AnimateOnView animation="reveal">
-        <p className="my-1 w-full text-3xl font-bold lg:my-3 lg:w-[90%]">
+        <p className="my-1 w-full text-2xl leading-[1.5] font-bold lg:my-3 lg:w-[90%] lg:text-3xl">
           세종대 동아리들을 한눈에 확인해요.
         </p>
       </AnimateOnView>
       <AnimateOnView animation="reveal-rightToleft">
-        <span className="my-1 block w-full text-lg font-semibold text-[#9C9C9C] lg:my-3 lg:w-[90%] lg:text-2xl">
+        <span className="text-md my-1 block w-full leading-[1.5] font-semibold text-[#9C9C9C] lg:my-3 lg:w-[90%] lg:text-2xl">
           간단한 동아리 설명과 동아리 카테고리를 확인할 수 있어요.
         </span>
       </AnimateOnView>
--- src/entities/home/ui/comment-card.tsx
+++ src/entities/home/ui/comment-card.tsx
@@ -5,35 +5,35 @@ function CommentCard() {
   return (
     <div className="relative h-[275px] w-full">
       <AnimateOnView animation="reveal-0">
-        <div className="absolute top-[15%] left-[5%]">
+        <div className="absolute top-[10%] lg:top-[5%] lg:left-[20%]">
           <Image
             src="/main/불면증염소.svg"
             alt="댓글 이미지 1"
             width={230}
             height={230}
-            className="lg:h-[400px] lg:w-[400px]"
+            className="w-[40%] lg:w-[300px]"
           />
         </div>
       </AnimateOnView>
       <AnimateOnView animation="reveal-1">
-        <div className="absolute top-[45%] left-[15%] z-10 lg:top-[35%] lg:left-[25%]">
+        <div className="absolute top-[20%] left-[15%] z-10 lg:top-[30%] lg:left-[35%]">
           <Image
             src="/main/격투기선수.svg"
             alt="댓글 이미지 2"
             width={300}
             height={300}
-            className="lg:h-[400px] lg:w-[400px]"
+            className="w-[70%] lg:w-[450px]"
           />
         </div>
       </AnimateOnView>
       <AnimateOnView animation="reveal-2">
-        <div className="absolute top-[5%] left-[30%] z-20 lg:left-[45%]">
+        <div className="absolute top-[7%] left-[38%] z-20 lg:left-[55%]">
           <Image
             src="/main/졸린사자.svg"
             alt="댓글 이미지 2"
             width={300}
             height={300}
-            className="lg:h-[400px] lg:w-[400px]"
+            className="w-[110%] lg:w-[500px]"
           />
         </div>
       </AnimateOnView>

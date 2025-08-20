83c1b672d1a16566f710fc48456b6bde9be0b1a2
신혁수 <122273080+sins051301@users.noreply.github.com>
2025-08-20 22:49:18 +0900

    [no-issue] fix: 즐찾 ui 수정

--- src/shared/ui/pagination.tsx
+++ src/shared/ui/pagination.tsx
@@ -14,32 +14,50 @@ export default function Pagination({ page, size, total }: PaginationProps) {
   const totalPages = Math.ceil(total / size);
 
   return (
-    <div className="mt-6 flex gap-2">
+    <div className="mt-8 flex gap-2">
       {page > 1 ? (
         <Button
           asChild
-          className="h-[40px] w-[40px] rounded-full border border-[#CFCFCF] bg-white"
+          className="h-[40px] w-[40px] rounded-full border-1 border-[#CFCFCF] bg-white text-2xl"
         >
-          <Link href={`?page=${page - 1}&size=${size}`}>
-            <Image src="/favorite/prev.svg" alt="prev" width={13} height={13} />
+          <Link href={`?page=${page - 1}&size=${size}`} className="text-2xl">
+            <Image
+              src="/favorite/prev.svg"
+              alt="prev"
+              width={60}
+              height={60}
+              className="scale-125"
+            />
           </Link>
         </Button>
       ) : (
         <Button
           disabled
-          className="h-[40px] w-[40px] cursor-not-allowed rounded-full border border-[#CFCFCF] bg-white opacity-50"
+          className="h-[40px] w-[40px] cursor-not-allowed rounded-full border-1 border-[#CFCFCF] bg-white text-2xl opacity-50"
         >
-          <Image src="/favorite/prev.svg" alt="prev" width={13} height={13} />
+          <Image
+            src="/favorite/prev.svg"
+            alt="prev"
+            width={60}
+            height={60}
+            className="scale-125"
+          />
         </Button>
       )}
 
       {page < totalPages ? (
         <Button
           asChild
-          className="h-[40px] w-[40px] rounded-full border border-[#CFCFCF] bg-white"
+          className="h-[40px] w-[40px] rounded-full border border-[#CFCFCF] bg-white text-2xl"
         >
-          <Link href={`?page=${page + 1}&size=${size}`}>
-            <Image src="/favorite/next.svg" alt="next" width={13} height={13} />
+          <Link href={`?page=${page + 1}&size=${size}`} className="text-2xl">
+            <Image
+              src="/favorite/next.svg"
+              alt="next"
+              width={60}
+              height={60}
+              className="scale-125"
+            />
           </Link>
         </Button>
       ) : (
@@ -47,7 +65,13 @@ export default function Pagination({ page, size, total }: PaginationProps) {
           disabled
           className="h-[40px] w-[40px] cursor-not-allowed rounded-full border border-[#CFCFCF] bg-white opacity-50"
         >
-          <Image src="/favorite/next.svg" alt="next" width={13} height={13} />
+          <Image
+            src="/favorite/next.svg"
+            alt="next"
+            width={60}
+            height={60}
+            className="scale-125"
+          />
         </Button>
       )}
     </div>

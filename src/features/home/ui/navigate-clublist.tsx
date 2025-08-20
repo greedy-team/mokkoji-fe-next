951d3aed7dcbc208d0b80a1928f49f16185ab1a8
ChangwooJ <wjdckddn99@naver.com>
2025-08-20 18:21:30 +0900

    [#122] refactor: 동아리 찾아보기 버튼 리팩토링

--- src/features/home/ui/navigate-clublist.tsx
+++ src/features/home/ui/navigate-clublist.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@/shared/ui/button';
-import { ArrowRight } from 'lucide-react';
+import Image from 'next/image';
 import Link from 'next/link';
 
 function NavigateClubList() {
@@ -7,12 +7,17 @@ function NavigateClubList() {
     <Button
       asChild
       variant="none"
-      size="lg"
-      className="flex cursor-pointer items-center gap-1.5 rounded-full bg-gradient-to-r from-[#00E804] to-[#33E3D0] text-base font-semibold text-white shadow-[0_0_8px_rgba(0,0,0,0.2)] transition hover:brightness-105"
+      size="navi"
+      className="flex cursor-pointer items-center gap-1.5 rounded-full bg-gradient-to-r from-[#00E804] to-[#33E3D0] text-[20px] font-semibold text-white shadow-[0_0_8px_rgba(0,0,0,0.2)] transition hover:brightness-105"
     >
-      <Link href="/club" className="flex items-center gap-1.5">
+      <Link href="/club" className="flex items-center gap-2.5">
         동아리 찾아보기
-        <ArrowRight />
+        <Image
+          src="/main/arrow.svg"
+          alt="동아리 찾아보기 버튼"
+          width={18}
+          height={14}
+        />
       </Link>
     </Button>
   );

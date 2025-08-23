98c9469c7047a08cee2046b7a5d29da331529baf
ChangwooJ <wjdckddn99@naver.com>
2025-08-24 02:52:29 +0900

    [#169] refactor: button 수정

--- src/features/home/ui/navigate-clublist.tsx
+++ src/features/home/ui/navigate-clublist.tsx
@@ -8,15 +8,16 @@ function NavigateClubList() {
       asChild
       variant="none"
       size="navi"
-      className="text-md flex cursor-pointer items-center gap-1.5 rounded-full bg-gradient-to-r from-[#00E804] to-[#33E3D0] font-semibold text-white shadow-[0_0_8px_rgba(0,0,0,0.2)] transition hover:brightness-105 lg:text-[20px]"
+      className="flex cursor-pointer items-center gap-1.5 rounded-full bg-gradient-to-r from-[#00E804] to-[#33E3D0] px-2 text-sm font-semibold text-white shadow-[0_0_8px_rgba(0,0,0,0.2)] transition hover:brightness-105 lg:text-[20px]"
     >
-      <Link href="/recruit" className="flex items-center gap-2.5">
+      <Link href="/recruit" className="flex items-center lg:gap-2.5">
         모집공고 찾아보기
         <Image
           src="/main/arrow.svg"
           alt="모집공고 찾아보기 버튼"
           width={18}
           height={14}
+          className="scale-60 lg:scale-100"
         />
       </Link>
     </Button>

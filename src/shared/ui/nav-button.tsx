d62c203ed8d4c8547b5736401f43daf48c88a114
ChangwooJ <wjdckddn99@naver.com>
2025-08-24 02:45:44 +0900

    [#169] refactor: 모바일 헤더 수정

--- src/shared/ui/nav-button.tsx
+++ src/shared/ui/nav-button.tsx
@@ -26,10 +26,10 @@ function NavButton({ label, navProps, href, onItemClick }: NavButtonProps) {
       href={href}
       onClick={onItemClick}
       className={clsx(
-        'relative z-[70] mx-1 flex h-full items-center px-2 no-underline transition-colors duration-500 lg:mx-1 lg:px-2 lg:py-2',
+        'z-[70] mx-1 flex h-full items-center px-2 no-underline transition-colors duration-500 lg:mx-1 lg:px-2 lg:py-2',
         navProps,
         active
-          ? 'border-b-3 border-black font-extrabold'
+          ? 'ml-2 border-black font-extrabold lg:ml-0 lg:border-b-3'
           : 'border-b-2 border-transparent',
         'hover:border-black',
       )}

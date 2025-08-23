6eba8bb02f81acd17d277254f5734193a54754f3
신혁수 <122273080+sins051301@users.noreply.github.com>
2025-08-23 21:20:12 +0900

    [#159] fix: 각종 오류 수정

--- scripts/dev-to-do.ts
+++ scripts/dev-to-do.ts
@@ -1,4 +1,3 @@
-// scripts/insert-devtodo.ts
 import fs from 'fs';
 import path from 'path';
 
--- src/app/(main)/club/layout.tsx
+++ src/app/(main)/club/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-import DevTodo from '@/shared/ui/dev-to-do';
 
 export const metadata: Metadata = {
   title: '모꼬지 | 세종대 동아리',
@@ -19,24 +18,6 @@ export default function Layout({
   return (
     <main className="flex flex-col items-center justify-center px-4 pt-4 lg:pt-10">
       {children}
-
-      <DevTodo
-        id="15344eb7-e1e6-403a-9c91-7fff8afb3eaf"
-        name="sins051301"
-        description="이거 수정"
-        todos={['test1', 'test2']}
-        x={324}
-        y={477}
-      />
-
-      <DevTodo
-        id="500ac5bd-8357-490c-92b5-9588f99f7cce"
-        name="sins051301"
-        description="이거다"
-        todos={['test1', 'test2']}
-        x={1001}
-        y={228}
-      />
     </main>
   );
 }

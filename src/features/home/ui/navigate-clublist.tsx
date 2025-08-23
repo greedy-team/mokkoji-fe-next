b0f2a765ea9ab5c9003367e7ecf54c9c97cc9b6e
ChangwooJ <wjdckddn99@naver.com>
2025-08-24 01:37:34 +0900

    [#160] refactor: 메인 페이지 이동 버튼 변경

--- src/features/home/ui/navigate-clublist.tsx
+++ src/features/home/ui/navigate-clublist.tsx
@@ -11,10 +11,10 @@ function NavigateClubList() {
       className="text-md flex cursor-pointer items-center gap-1.5 rounded-full bg-gradient-to-r from-[#00E804] to-[#33E3D0] font-semibold text-white shadow-[0_0_8px_rgba(0,0,0,0.2)] transition hover:brightness-105 lg:text-[20px]"
     >
       <Link href="/recruit" className="flex items-center gap-2.5">
-        동아리 찾아보기
+        모집공고 찾아보기
         <Image
           src="/main/arrow.svg"
-          alt="동아리 찾아보기 버튼"
+          alt="모집공고 찾아보기 버튼"
           width={18}
           height={14}
         />

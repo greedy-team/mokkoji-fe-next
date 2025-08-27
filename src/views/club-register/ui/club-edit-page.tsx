c3e29c36e2e206d461ce0cf017a1477d906b4ea0
신혁수 <122273080+sins051301@users.noreply.github.com>
2025-08-28 00:15:16 +0900

    [no-issue] fix: 동아리 권한 버그 수정

--- src/views/club-register/ui/club-edit-page.tsx
+++ src/views/club-register/ui/club-edit-page.tsx
@@ -1,8 +1,17 @@
 import { DetailParams } from '@/shared/model/type';
+import SharedLoading from '@/shared/ui/loading';
 import ClubEditWidget from '@/widgets/club-register/ui/club-edit-widget';
+import { Suspense } from 'react';
 
 function ClubEditPage({ params }: DetailParams) {
-  return <ClubEditWidget params={params} />;
+  return (
+    <div className="w-[90%] lg:w-[30%]">
+      <h1 className="my-6 text-2xl font-bold">동아리 정보 수정</h1>
+      <Suspense fallback={<SharedLoading />}>
+        <ClubEditWidget params={params} />
+      </Suspense>
+    </div>
+  );
 }
 
 export default ClubEditPage;

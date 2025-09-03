964d2097c8254eb20492ce2957cd3ce0932407d4
신혁수 <122273080+sins051301@users.noreply.github.com>
2025-09-03 20:50:24 +0900

    [#190] refactor: next-auth 개선

--- src/shared/api/auth-api.ts
+++ src/shared/api/auth-api.ts
@@ -3,24 +3,45 @@
 import 'server-only';
 import ky from 'ky';
 import { auth } from '@/auth';
+import serverApi from './server-api';
 
 async function authAPi() {
-  const session = await auth();
-
-  if (!session?.accessToken) {
-    return ky.create({
-      prefixUrl: process.env.NEXT_PUBLIC_API_URL,
-    });
-  }
-
   return ky.create({
     prefixUrl: process.env.NEXT_PUBLIC_API_URL,
     hooks: {
       beforeRequest: [
         async (req) => {
-          if (session.accessToken && !req.headers.get('Authorization')) {
+          if (!req.headers.get('Authorization')) {
+            const session = await auth();
+            if (!session || !session.accessToken) return req;
             req.headers.set('Authorization', `Bearer ${session.accessToken}`);
+            return req;
           }
+          return req;
+        },
+      ],
+      afterResponse: [
+        async (request, options, response) => {
+          if (response.status !== 401) return response;
+          const session = await auth();
+          if (!session || !session.refreshToken) return response;
+
+          const refreshResponse = await serverApi.post('users/auth/refresh', {
+            headers: {
+              Authorization: `Bearer ${session.refreshToken}`,
+            },
+          });
+          console.log('refreshResponse!', refreshResponse);
+
+          const refreshData: { data: { accessToken: string } } =
+            await refreshResponse.json();
+
+          request.headers.set(
+            'Authorization',
+            `Bearer ${refreshData.data.accessToken}`,
+          );
+
+          return ky(request);
         },
       ],
     },

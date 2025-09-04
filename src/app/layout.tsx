ab56fe7122e35864fa70c98eb46c8167418bbea5
신혁수 <122273080+sins051301@users.noreply.github.com>
2025-09-04 16:22:15 +0900

    [no-issue] fix: 폰트 woff2로 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,39 +6,39 @@ import BuildInsightProvider from '@/_providers/build-insight-provider';
 const pretendard = localFont({
   src: [
     {
-      path: '../shared/fonts/Pretendard-Thin.ttf',
+      path: '../shared/fonts/Pretendard-Thin.woff2',
       weight: '100',
     },
     {
-      path: '../shared/fonts/Pretendard-ExtraLight.ttf',
+      path: '../shared/fonts/Pretendard-ExtraLight.woff2',
       weight: '200',
     },
     {
-      path: '../shared/fonts/Pretendard-Light.ttf',
+      path: '../shared/fonts/Pretendard-Light.woff2',
       weight: '300',
     },
     {
-      path: '../shared/fonts/Pretendard-Regular.ttf',
+      path: '../shared/fonts/Pretendard-Regular.woff2',
       weight: '400',
     },
     {
-      path: '../shared/fonts/Pretendard-Medium.ttf',
+      path: '../shared/fonts/Pretendard-Medium.woff2',
       weight: '500',
     },
     {
-      path: '../shared/fonts/Pretendard-SemiBold.ttf',
+      path: '../shared/fonts/Pretendard-SemiBold.woff2',
       weight: '600',
     },
     {
-      path: '../shared/fonts/Pretendard-Bold.ttf',
+      path: '../shared/fonts/Pretendard-Bold.woff2',
       weight: '700',
     },
     {
-      path: '../shared/fonts/Pretendard-ExtraBold.ttf',
+      path: '../shared/fonts/Pretendard-ExtraBold.woff2',
       weight: '800',
     },
     {
-      path: '../shared/fonts/Pretendard-Black.ttf',
+      path: '../shared/fonts/Pretendard-Black.woff2',
       weight: '900',
     },
   ],

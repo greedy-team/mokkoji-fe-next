c17f3263b8c4f9030e00af28520265443dc80a89
신혁수 <122273080+sins051301@users.noreply.github.com>
2025-10-09 19:09:01 +0900

    Updates

--- assets/preview-D63jI-8B.js
+++
@@ -1,1 +0,0 @@
-const e={parameters:{controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}},viewport:{viewports:{mobile:{name:"Mobile",styles:{width:"375px",height:"812px"}},tablet:{name:"Tablet",styles:{width:"768px",height:"1024px"}},desktop:{name:"Desktop",styles:{width:"1280px",height:"800px"}}},defaultViewport:"responsive"}}};export{e as default};

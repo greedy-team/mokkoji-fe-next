fa0aae12b1c2cc8790a5ccf39d87ee0012ed58d9
신혁수 <122273080+sins051301@users.noreply.github.com>
2025-10-28 16:02:13 +0900

    [no-issue] fix: nuqs 롤백

--- src/app/(main)/club/page.tsx
+++ src/app/(main)/club/page.tsx
@@ -1,8 +1,6 @@
 import ClubPage from '@/views/club/ui/club-page';
 import { RecruitItemListProps } from '@/widgets/recruit/model/type';
 
-export const revalidate = 1800;
-
 function Page({ searchParams }: RecruitItemListProps) {
   return <ClubPage searchParams={searchParams} />;
 }
--- src/app/(main)/recruit/page.tsx
+++ src/app/(main)/recruit/page.tsx
@@ -1,8 +1,6 @@
 import RecruitPage from '@/views/recruit/ui/recruit-page';
 import { RecruitmentSearchParams } from '@/shared/model/recruit-type';
 
-export const revalidate = 1800;
-
 function Page({
   searchParams,
 }: {

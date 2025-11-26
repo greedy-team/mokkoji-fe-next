b3e4da6495e2b6ee86bb4b832159f86b3d51ca12
신혁수 <122273080+sins051301@users.noreply.github.com>
2025-11-26 20:50:33 +0900

    [no-issue] fix: 필요없는 거 수정

--- src/app/(main)/club/[id]/search-params.ts
+++
@@ -1,7 +0,0 @@
-import { createSearchParamsCache, parseAsString } from 'nuqs/server';
-
-export const searchParamsCache = createSearchParamsCache({
-  tab: parseAsString.withDefault(''),
-});
-
-export type FavoriteSearchParams = ReturnType<typeof searchParamsCache.parse>;

b2bfc05edfd49c646d8d452c981d6a107ec06d51
ChangwooJ <127019318+ChangwooJ@users.noreply.github.com>
2026-01-04 13:16:17 +0900

    [#283] feat: 스텝별 모집글 작성 구성

---
+++ src/features/admin-recruitment/ui/steps/step-base-recruitment.tsx
@@ -0,0 +1,127 @@
+import DateRangePicker from '@/shared/ui/calender/date-range-picker';
+import Input from '@/shared/ui/input';
+import { ClubInfoType } from '@/shared/model/type';
+import ImageUploadSection from '@/shared/ui/image-upload-section';
+import { RecruitmentFormData } from '../../model/type';
+
+interface ImageItem {
+  id: string;
+  file: File;
+  previewUrl: string;
+  imageName: string;
+}
+
+interface BaseRecruitmentProps {
+  formData: RecruitmentFormData;
+  errors: Partial<Record<keyof RecruitmentFormData, string>>;
+  onChange: <K extends keyof RecruitmentFormData>(
+    field: K,
+    value: RecruitmentFormData[K],
+  ) => void;
+  onBlur: (field: keyof RecruitmentFormData) => void;
+  clubInfo?: ClubInfoType;
+  imageFiles: ImageItem[];
+  handleImageChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleImageRemove: (id: string) => void;
+  inputRef: React.RefObject<HTMLInputElement | null>;
+  handleDragStart: (id: string) => void;
+  handleDragOver: (
+    e: React.DragEvent<HTMLDivElement>,
+    targetId: string,
+  ) => void;
+  handleDragEnd: () => void;
+  draggingId: string | null;
+  onDragOver: (e: React.DragEvent<HTMLDivElement>) => void;
+  onDrop: (e: React.DragEvent<HTMLDivElement>) => void;
+}
+
+function StepBaseRecruitment({
+  formData,
+  errors,
+  onChange,
+  onBlur,
+  clubInfo,
+  imageFiles,
+  handleImageChange,
+  handleImageRemove,
+  inputRef,
+  handleDragStart,
+  handleDragOver,
+  handleDragEnd,
+  draggingId,
+  onDragOver,
+  onDrop,
+}: BaseRecruitmentProps) {
+  return (
+    <>
+      <label htmlFor="name" className="mt-4 text-base font-semibold">
+        동아리 이름
+      </label>
+      <Input id="name" name="name" value={clubInfo?.name} disabled />
+      <label
+        htmlFor="title"
+        className="mt-4 flex gap-2 text-base font-semibold"
+      >
+        모집글 제목
+        {errors.title && (
+          <p className="pt-1 text-xs font-medium text-red-500">
+            {errors.title}
+          </p>
+        )}
+      </label>
+      <Input
+        id="title"
+        name="title"
+        value={formData.title}
+        variant={errors.title ? 'error' : 'default'}
+        onChange={(e) => onChange('title', e.target.value)}
+        onBlur={() => onBlur('title')}
+      />
+      <label
+        htmlFor="recruitForm"
+        className="mt-4 flex gap-2 text-base font-semibold"
+      >
+        모집폼 링크
+        {errors.recruitForm && (
+          <p className="pt-1 text-xs font-medium text-red-500">
+            {errors.recruitForm}
+          </p>
+        )}
+      </label>
+      <Input
+        id="recruitForm"
+        name="recruitForm"
+        placeholder="https://example.com"
+        value={formData.recruitForm}
+        variant={errors.recruitForm ? 'error' : 'default'}
+        onChange={(e) => onChange('recruitForm', e.target.value)}
+        onBlur={() => onBlur('recruitForm')}
+      />
+      <DateRangePicker
+        startDate={formData.recruitStart}
+        endDate={formData.recruitEnd}
+        onStartDateChange={(date) => onChange('recruitStart', date)}
+        onEndDateChange={(date) => onChange('recruitEnd', date)}
+        onRangeComplete={() => {
+          onBlur('recruitStart');
+          onBlur('recruitEnd');
+        }}
+        label="모집 기간"
+      />
+      <ImageUploadSection
+        imageFiles={imageFiles}
+        handleImageRemove={handleImageRemove}
+        handleImageChange={handleImageChange}
+        inputRef={inputRef}
+        handleDragStart={handleDragStart}
+        handleDragOver={handleDragOver}
+        handleDragEnd={handleDragEnd}
+        draggingId={draggingId}
+        onDragOver={onDragOver}
+        onDrop={onDrop}
+      />
+    </>
+  );
+}
+
+export default StepBaseRecruitment;

c8969e315ec64ec46ef16f9d7a2d5bae9d229aa8
ChangwooJ <127019318+ChangwooJ@users.noreply.github.com>
2026-01-16 23:47:56 +0900

    [#383] feat: 동아리 정보 수정 퍼널 추가

--- src/widgets/admin-club-description/admin-club-description-widget.tsx
+++ src/widgets/admin-club-description/admin-club-description-widget.tsx
@@ -1,16 +1,18 @@
 import { getClubInfo } from '@/shared/api/manage-api';
 import { RecruitmentActionParams } from '@/shared/model/type';
 import ErrorBoundaryUi from '@/shared/ui/error-boundary-ui';
+import EditFlowContainer from '@/features/admin-club-description/ui/edit/edit-flow-container';
 
 async function AdminClubDescriptionWidget({ params }: RecruitmentActionParams) {
-  const { action, id } = await params;
-  const res = await getClubInfo(Number(id));
+  const { id } = await params;
+  const clubId = Number(id);
+  const res = await getClubInfo(clubId);
 
-  if (!res.ok) {
+  if (!res.ok || !res.data) {
     return <ErrorBoundaryUi />;
   }
 
-  return <div>준비중입니다.</div>;
+  return <EditFlowContainer clubInfo={res.data} clubId={clubId} />;
 }
 
 export default AdminClubDescriptionWidget;

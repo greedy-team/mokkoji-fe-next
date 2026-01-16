d6e91020fe25a15415ef536dd55c554918e2dece
zldn109 <ss771009@naver.com>
2026-01-17 00:21:51 +0900

    [#386] feat: 작성일 형식 0000년 00월 00일로 출력

--- src/entities/club-detail/ui/recruit-detail-header.tsx
+++ src/entities/club-detail/ui/recruit-detail-header.tsx
@@ -31,46 +31,49 @@ function RecruitDetailHeader({
   status,
 }: RecruitDetailHeaderProps) {
   const [date, time] = (createdAt || '').split('T');
+  const [year, month, day] = date.split('-');
+  const formattedDate = `${year}년 ${month}월 ${day}일`;
+
   return (
-    <>
-      <header className="w-full cursor-default">
-        <div className="mb-4 flex flex-row items-center gap-2.5 lg:gap-5">
-          <ClickLogo logo={logo} title={title} />
-          <h1 className="text-xl font-bold whitespace-nowrap lg:text-4xl">
-            {title}
-          </h1>
-          <p className="text-lg font-bold whitespace-nowrap text-[#9C9C9C] lg:text-3xl">
-            <Link href={`/recruit?category=${ClubCategoryToLabel[category]}`}>
-              {category} 동아리
-            </Link>
-          </p>
-        </div>
-        <div className="mb-4 flex flex-row items-center gap-4 lg:text-xl">
-          <RadiusTag
-            status={status}
-            className="whitespace-nowrap lg:text-[16px]"
-          />
+    <header className="w-full cursor-default">
+      <div className="mb-4 flex flex-row items-center gap-2.5 lg:gap-5">
+        <ClickLogo logo={logo} title={title} />
+        <h1 className="text-xl font-bold whitespace-nowrap lg:text-4xl">
+          {title}
+        </h1>
+        <p className="text-lg font-bold whitespace-nowrap text-[#9C9C9C] lg:text-3xl">
+          <Link href={`/recruit?category=${ClubCategoryToLabel[category]}`}>
+            {category} 동아리
+          </Link>
+        </p>
+      </div>
+      <div className="slg:text-xl mb-4 flex gap-4">
+        <RadiusTag
+          status={status}
+          className="whitespace-nowrap lg:text-[16px]"
+        />
+        <div className="flex flex-col">
           <PeriodSection
             startDate={startDate}
             endDate={endDate}
             decoration={false}
             className="whitespace-nowrap lg:text-lg"
           />
+          <div className="flex">
+            {date && (
+              <p className="lg:text-md mb-2 text-sm text-[#9C9C9C]">
+                작성일 · {formattedDate}
+              </p>
+            )}
+          </div>
         </div>
-        <div className="flex justify-between">
-          {date && (
-            <p className="lg:text-md mb-2 text-sm text-[#9C9C9C]">
-              작성일: {date} {time}
-            </p>
-          )}
-        </div>
-      </header>
-      <RecruitDetailHeaderControl
-        instagram={instagram}
-        clubId={clubId}
-        isFavorite={isFavorite || false}
-      />
-    </>
+        <RecruitDetailHeaderControl
+          instagram={instagram}
+          clubId={clubId}
+          isFavorite={isFavorite || false}
+        />
+      </div>
+    </header>
   );
 }
 

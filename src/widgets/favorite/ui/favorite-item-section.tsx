d4cc13801f7f7c43738836c2a186f6bc16aeb1ff
ChangwooJ <127019318+ChangwooJ@users.noreply.github.com>
2026-01-26 22:06:02 +0900

    [#415] fix: 로그인 필요 서비스 문구 수정

--- src/widgets/favorite/ui/favorite-item-section.tsx
+++ src/widgets/favorite/ui/favorite-item-section.tsx
@@ -1,4 +1,3 @@
-import { auth } from '@/auth';
 import ErrorBoundaryUi from '@/shared/ui/error-boundary-ui';
 import getFavoriteList from '../api/getFavoriteList';
 import FavoriteItemList from './favorite-item-list';
@@ -6,16 +5,6 @@ import FavoriteItemList from './favorite-item-list';
 const MAX_FAVORITE_SIZE = 100;
 
 async function FavoriteItemSection() {
-  const session = await auth();
-
-  if (!session) {
-    return (
-      <h1 className="mt-10 mb-5 text-sm font-bold text-[#00E457] lg:text-2xl">
-        로그인 후 이용하실 수 있습니다.
-      </h1>
-    );
-  }
-
   const data = await getFavoriteList({ page: 1, size: MAX_FAVORITE_SIZE });
 
   if (!data.ok || !data.data) {

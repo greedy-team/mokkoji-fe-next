447805e7dda6971a4b8aaed79299141151de293b
ChangwooJ <127019318+ChangwooJ@users.noreply.github.com>
2026-01-27 15:21:09 +0900

    [#427] feat: 메인페이지 서브 기능 안내 섹션 모바일 적용

--- src/views/home/ui/mobile-home-page.tsx
+++ src/views/home/ui/mobile-home-page.tsx
@@ -1,10 +1,10 @@
 import MobileHomeHeader from '@/entities/home/ui/mobile-home-header';
 import NavigateClubList from '@/features/home/ui/navigate-clublist';
 import HomeDownButton from '@/features/home/ui/page-down-button';
-import SharedLoading from '@/shared/ui/loading';
 import ClubCardMobileWidget from '@/widgets/home/ui/club-card-mobile-widget';
+import FeatureIntroduceMobileWidget from '@/widgets/home/ui/feature-introduce-mobile-widget';
+import HelpCardMobileWidget from '@/widgets/home/ui/help-card-mobile-widget';
 import HomeSearchWidget from '@/widgets/home/ui/home-search-widget';
-import { Suspense } from 'react';
 
 function MobileHomePage() {
   return (
@@ -18,6 +18,12 @@ function MobileHomePage() {
       </div>
       <section className="px-5">
         <ClubCardMobileWidget />
+      </section>
+      <section className="bg-[linear-gradient(to_bottom,_#FFFFFF_0%,_#F8FAFB_5%,_#F8FAFB_95%,_#FFFFFF_100%)] px-5">
+        <FeatureIntroduceMobileWidget />
+      </section>
+      <section className="px-5">
+        <HelpCardMobileWidget />
       </section>
       <div className="fixed right-2 bottom-24 z-50">
         <NavigateClubList />

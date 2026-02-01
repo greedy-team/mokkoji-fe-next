9a30a75f229e23b1f80c5213e887f4ac1e55a1c2
zldn109 <ss771009@naver.com>
2026-02-02 01:59:45 +0900

    [#445] fix: 동아리 권한이 있는 사용자에 대해 뜨던 수정하기, 삭제하기 버튼 제거

--- src/widgets/club-detail/ui/club-recruit-widget.tsx
+++ src/widgets/club-detail/ui/club-recruit-widget.tsx
@@ -37,80 +37,24 @@ function ClubRecruitWidget({
   rid,
   recruitDetail,
 }: ClubRecruitWidgetProps) {
-  const [isEditing, setIsEditing] = useState(false);
-
   const router = useRouter();
-
-  const handleDelete = async (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    const confirmDelete = window.confirm('모집 공고를 삭제하시겠습니까?');
-    if (!confirmDelete) return;
-
-    const res = await deleteRecruitmentForm(clubId);
-
-    if (!res.ok) {
-      toast.error(res.message);
-      return;
-    }
-
-    if (Array.isArray(res.data?.deleteImageUrls)) {
-      await Promise.all(
-        res.data.deleteImageUrls.map((url: string) => ky.delete(url)),
-      );
-    }
-    toast.success('모집 공고 삭제가 성공적으로 업로드되었습니다!');
-    router.push('/recruit');
-  };
 
   return (
     <>
-      {isManageClub && (
-        <div className="mb-6 flex cursor-pointer justify-end gap-4 text-base text-[#9C9C9C] lg:mb-0">
-          <button
-            onClick={() => setIsEditing(true)}
-            className="cursor-pointer text-[#00E457] hover:underline"
-          >
-            수정하기
-          </button>
-          <button
-            onClick={handleDelete}
-            className="cursor-pointer text-red-400 hover:underline"
-          >
-            삭제하기
-          </button>
-        </div>
-      )}
-      {!isEditing ? (
-        <>
-          <RecruitDetailView
-            title={recruitDetail.title}
-            content={recruitDetail.content}
-            recruitForm={recruitDetail.recruitForm}
-            imageUrls={recruitDetail.imageUrls}
-          />
-          <RecruitHistorySection
-            clubId={clubId}
-            recruitHistories={recruitHistories}
-            selectedRid={rid}
-          />
-          <div className="fixed right-2 bottom-5 z-50 lg:right-[calc((100vw-60vw)/2+24px)] lg:bottom-8 lg:bottom-14">
-            <NavigateRecruitForm recruitForm={recruitDetail.recruitForm} />
-          </div>
-        </>
-      ) : (
-        <ClubDetailRecruitmentEdit
-          title={recruitDetail.title}
-          clubName={recruitDetail.clubName}
-          category={recruitDetail.category}
-          content={recruitDetail.content}
-          recruitForm={recruitDetail.recruitForm}
-          recruitStart={recruitDetail.recruitStart}
-          recruitEnd={recruitDetail.recruitEnd}
-          clubId={clubId}
-          setIsEditing={setIsEditing}
-          imageUrls={recruitDetail.imageUrls}
-        />
-      )}
+      <RecruitDetailView
+        title={recruitDetail.title}
+        content={recruitDetail.content}
+        recruitForm={recruitDetail.recruitForm}
+        imageUrls={recruitDetail.imageUrls}
+      />
+      <RecruitHistorySection
+        clubId={clubId}
+        recruitHistories={recruitHistories}
+        selectedRid={rid}
+      />
+      <div className="fixed right-2 bottom-5 z-50 lg:right-[calc((100vw-60vw)/2+24px)] lg:bottom-8 lg:bottom-14">
+        <NavigateRecruitForm recruitForm={recruitDetail.recruitForm} />
+      </div>
     </>
   );
 }

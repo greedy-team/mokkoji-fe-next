f57d29d45295931a31e7c2d1f3458334817550df
ChangwooJ <127019318+ChangwooJ@users.noreply.github.com>
2026-02-07 14:03:57 +0900

    [#452] fix: 어드민 메뉴 버튼 포인터 커서

--- src/features/admin/ui/components/menu-button.tsx
+++ src/features/admin/ui/components/menu-button.tsx
@@ -9,7 +9,11 @@ interface MenuButtonProps {
 
 function MenuButton({ label, onClick }: MenuButtonProps) {
   return (
-    <button type="button" className="flex gap-2 lg:gap-3.5" onClick={onClick}>
+    <button
+      type="button"
+      className="flex cursor-pointer gap-2 lg:gap-3.5"
+      onClick={onClick}
+    >
       <span className="text-base font-bold lg:text-2xl">{label}</span>
       <Image
         src="/admin/arrow.svg"

34e402357082997e39ac402d098fef39a64a060b
ChangwooJ <127019318+ChangwooJ@users.noreply.github.com>
2026-02-09 16:23:57 +0900

    [#452] fix: 중복 css 제거

--- src/shared/ui/textarea.tsx
+++ src/shared/ui/textarea.tsx
@@ -28,7 +28,7 @@ function Textarea({
       disabled={disabled}
       data-slot="textarea"
       className={cn(
-        'scrollbar-custom placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex min-h-[200px] w-full min-w-0 resize-none rounded-[10px] border-2 bg-transparent px-3 py-2 text-sm transition-[color,box-shadow] outline-none disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+        'scrollbar-custom placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex min-h-[200px] w-full min-w-0 resize-none rounded-[10px] border-2 bg-transparent px-3 py-2 text-sm transition-[color,box-shadow] outline-none disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50',
         'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
         variantClass[variant],
         className,
